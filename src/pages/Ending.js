527414823a26fc9d09f7d43be75f250b34300c8a
eunjeong <eunjeongJang@lotte.net>
2022-05-31 15:49:42 +0900

    엔딩에 loading status 추가

--- src/pages/Ending.js
+++ src/pages/Ending.js
@@ -4,10 +4,26 @@ import Footer from './Layout/Footer';
 import { Layout } from 'antd';
 import 'antd/dist/antd.min.css';
 import '../css/Game.css';
+import Loading from './Layout/Loading';
 const Ending = () => {
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   setLoading(false);
+  // });
+
+  //  props 안넘기면 엔딩 안보이게 하는 기능 추가
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
+
   return (
     <Layout style={{ height: '100%', backgroundColor: 'white' }}>
       <Header />
+      {loading ? <Loading /> : null}
       <div className="content-wrapper" style={{ height: '100%' }}>
         <p>엔딩입니다</p>
         {/*   To-do: 공유하기 기능 */}
---
+++ src/pages/Layout/Loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div>
+      <p>스피너</p>
+    </div>
+  );
+};
+
+export default Loading;
