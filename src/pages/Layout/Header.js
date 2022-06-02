4f21da2c91ccdfffb3d9c0a3328214cd5899885a
hayeonii <hayexn51@gmail.com>
2022-06-02 17:57:10 +0900

    Ending CSS 수정 & Header logo 위치 수정

--- src/pages/Layout/Header.js
+++ src/pages/Layout/Header.js
@@ -1,5 +1,5 @@
 import { Layout } from 'antd';
-import headerImg from '../../images/logo_icon.PNG';
+import headerImg from '../../images/logo_icon.png';
 
 const Header = () => {
   const { Header } = Layout;
